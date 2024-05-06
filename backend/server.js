2b7c54c8023880b06d714345fa159187bf93b676
shivarajkulal <shivarajkulalsn884@gmail.com>
2024-05-06 16:13:23 +0530

    Router Handling

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,12 @@ const app = express();
 const db = require("./db");
 require("dotenv").config();
 
+const cors = require("cors");
+app.use(cors());
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // req.body
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Import the router files
 const userRoutes = require("./routes/userRoutes");
@@ -16,5 +19,5 @@ app.use("/user", userRoutes);
 app.use("/candidate", candidateRoutes);
 
 app.listen(PORT, () => {
-  console.log("listening on port 3000");
+  console.log("listening on port 5000");
 });
